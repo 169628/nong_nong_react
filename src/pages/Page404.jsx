797a3e5cdf6e478a32d73df7cdfdc71f0a299471
Rainbow Lai <rainbow.pipi.1986@outlook.com>
2025-02-23 13:41:56 +0800

    one product page

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,15 @@
+function Page404() {
+  return (
+    <>
+      <div
+        className="container d-flex flex-column justify-content-center align-items-center"
+        style={{ height: "60vh" }}
+      >
+        <h1 className="text-primary-500">404</h1>
+        <p className="text-primary-500 fs-1 fw-700">找不到這一頁</p>
+      </div>
+    </>
+  );
+}
+
+export default Page404;
